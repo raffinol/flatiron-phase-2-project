0cdd4c2d996f213fee0f570d6b7fc5e3debbc7d3
Rafael Finol <rafaelfinol88@gmail.com>
2023-06-24 19:23:17 -0400

    create 'TestComponent' to test functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import TestComponent from './components/TestComponent';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <TestComponent />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/TestComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function TestComponent() {
+  return <div>Test Component</div>;
+}
+
+export default TestComponent;
