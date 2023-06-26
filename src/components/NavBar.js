4dbfba06898e591aaa93619182ea4efd4a1126ec
Rafael Finol <rafaelfinol88@gmail.com>
2023-06-25 21:32:30 -0400

    change Nav with NavLink

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function NavBar() {
   return (
     <nav>
-      <Link to="/">Patients</Link>
-      <Link to="/NewPatient">New Patient</Link>
+      <NavLink exact to="/">
+        Patients
+      </NavLink>
+      <NavLink to="/NewPatient">New Patient</NavLink>
     </nav>
   );
 }
