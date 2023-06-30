25b36461a21fa999b8fe89d4177dd2c209ee923f
Rafael Finol <rafaelfinol88@gmail.com>
2023-06-30 18:40:20 -0400

    add Route to ModifyPatient

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import PatientsList from './components/PatientsList';
 import NewPatient from './components/NewPatient';
 import NavBar from './components/NavBar';
+import ModifyPatient from './components/ModifyPatient';
 
 function App() {
   const [patients, setPatients] = useState([]);
@@ -26,6 +27,9 @@ function App() {
         </Route>
         <Route exact path="/NewPatient">
           <NewPatient onAddPatient={onAddPatient} />
+        </Route>
+        <Route exact path="/ModifyPatient">
+          <ModifyPatient />
         </Route>
         <Route path="*">
           <h1>404 not found</h1>
