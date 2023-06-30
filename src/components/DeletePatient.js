b6e045fe182a74d06c64a7a64212c188b5cf5f21
Rafael Finol <rafaelfinol88@gmail.com>
2023-06-30 11:02:09 -0400

    fetch Delete patient

--- src/components/DeletePatient.js
+++ src/components/DeletePatient.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-function DeletePatient() {
-  return <div>DeletePatient</div>;
+function DeletePatient({ id, patients, setPatients }) {
+  function handleDeleteClick() {
+    fetch(`http://localhost:3000/patients/${id}`, {
+      method: 'DELETE',
+    })
+      .then((r) => r.json())
+      .then(() => {
+        const updatedPatients = patients.filter((p) => p.id !== id);
+        setPatients(updatedPatients);
+      });
+  }
+  return (
+    <div>
+      <button onClick={handleDeleteClick}>Delete Patient</button>
+    </div>
+  );
 }
 
 export default DeletePatient;
