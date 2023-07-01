35786ab4fcc1cc8a5683e0725b643fd81a18c051
Rafael Finol <rafaelfinol88@gmail.com>
2023-07-01 13:36:30 -0400

    validate before showing PatientInfo

--- src/components/PatientsList.js
+++ src/components/PatientsList.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import PatientInfo from './PatientInfo';
 
 function PatientsList({ patients, setPatients }) {
-  console.log(patients);
   const [patient, setPatient] = useState('');
 
   const patientsMap = patients.map((patient) => (
@@ -10,11 +9,14 @@ function PatientsList({ patients, setPatients }) {
       {patient.name}
     </li>
   ));
+  console.log(patient);
   return (
     <div>
       <h3>Patients</h3>
       {patientsMap}
-      <PatientInfo patient={patient} setPatient={setPatient} patients={patients} setPatients={setPatients} />
+      {patient.length !== 0 && patient.name !== '' ? (
+        <PatientInfo patient={patient} setPatient={setPatient} patients={patients} setPatients={setPatients} />
+      ) : null}
     </div>
   );
 }
