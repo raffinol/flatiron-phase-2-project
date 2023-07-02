b22fab6840e3d20333311e318457d09a87e26854
Rafael Finol <rafaelfinol88@gmail.com>
2023-07-01 23:21:13 -0400

    Delete unused index.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 
