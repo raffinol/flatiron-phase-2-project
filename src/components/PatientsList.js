ffc5f541fd3c27c9ee014aac5e57dfdc564feb12
Rafael Finol <rafaelfinol88@gmail.com>
2023-07-01 23:15:18 -0400

    Format PatientList component

--- src/components/PatientsList.js
+++ src/components/PatientsList.js
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
 import PatientInfo from './PatientInfo';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 function PatientsList({ patients, setPatients }) {
   const [patient, setPatient] = useState('');
 
   const patientsMap = patients.map((patient) => (
-    <li onClick={() => setPatient(patient)} key={patient.id}>
-      {patient.name}
-    </li>
+    <ListGroup>
+      <ListGroup.Item action onClick={() => setPatient(patient)} key={patient.id}>
+        {patient.name}
+      </ListGroup.Item>
+    </ListGroup>
   ));
   console.log(patient);
   return (
     <div>
-      <h3>Patients</h3>
+      <h3 className="ms-4 mt-3">Patients List</h3>
       {patientsMap}
       {patient.length !== 0 && patient.name !== '' ? (
         <PatientInfo patient={patient} setPatient={setPatient} patients={patients} setPatients={setPatients} />
