7e1239bd7c8b317530801a7795dbaf0bc5cb18c4
Rafael Finol <rafaelfinol88@gmail.com>
2023-07-01 23:15:35 -0400

    Format PatientInfo component

--- src/components/PatientInfo.js
+++ src/components/PatientInfo.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import DeletePatient from './DeletePatient';
 import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Stack from 'react-bootstrap/Stack';
 
 function PatientInfo({ patient, setPatient, patients, setPatients }) {
   const { id, name, age, previousAppointment, nextAppointment } = patient;
   return (
-    <div>
-      <h3>Patient Info</h3>
-      <p>Name: {name}</p>
-      <p>Age: {age}</p>
-      <p>Previous Appointment: {previousAppointment}</p>
-      <p>Next Appointment: {nextAppointment}</p>
-      <DeletePatient id={id} patients={patients} setPatients={setPatients} setPatient={setPatient} />
-      <Link
-        to={{
-          pathname: '/ModifyPatient',
-          state: patient,
-        }}
-      >
-        <button>Modify Patient</button>
-      </Link>
-    </div>
+    <>
+      <Stack>
+        <h3 className="ms-4 mt-3">Patient Information</h3>
+        <p className="p-1 ms-3">Name: {name}</p>
+        <p className="p-1 ms-3">Age: {age}</p>
+        <p className="p-1 ms-3">Previous Appointment: {previousAppointment}</p>
+        <p className="p-1 ms-3">Next Appointment: {nextAppointment}</p>
+      </Stack>
+      <Stack className="ms-2 mt-2" direction="horizontal" gap={2}>
+        <Link
+          to={{
+            pathname: '/ModifyPatient',
+            state: patient,
+          }}
+        >
+          <Button variant="primary">Modify Patient</Button>
+        </Link>
+        <DeletePatient id={id} patients={patients} setPatients={setPatients} setPatient={setPatient} />
+      </Stack>
+    </>
   );
 }
 
